4247a7ee8328cb3a79ae4858302daccff3b6a14d
hyukjun52 <82699474+hyukjun52@users.noreply.github.com>
2025-09-11 20:25:27 +0900

    css update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,20 +16,24 @@ function App() {
         {/* 카드 영역 */}
         <div className="card-container">
           <div className="card">
-            <div className="card-header">ToDo</div>
-            <div className="card-body">할 일 리스트</div>
-          </div>
-          <div className="card">
             <div className="card-header">달력</div>
-            <div className="card-body">캘린더 위젯</div>
+            <div className="card-body">달력 위젯</div>
           </div>
           <div className="card">
             <div className="card-header">메모</div>
-            <div className="card-body">메모 내용</div>
+            <div className="card-body">메모 작성</div>
           </div>
           <div className="card">
+            <div className="card-header">ToDo</div>
+            <div className="card-body">할 일 리스트</div>
+          </div>
+          <div className="card wide-card">
             <div className="card-header">디데이</div>
             <div className="card-body">디데이 목록</div>
+          </div>
+          <div className="card">
+            <div className="card-header">날씨</div>
+            <div className="card-body">날씨 보기</div>
           </div>
         </div>
       </div>
